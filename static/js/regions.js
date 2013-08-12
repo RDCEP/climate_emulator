b6b9470a1db1499649120fdfa08fb50c58bce34a
Nathan Matteson <matteson@obstructures.org>
2013-08-12 11:39:41 -0500

    Initial commit

---
+++ static/js/regions.js
@@ -0,0 +1,11 @@
+var region_codes = ['ALA', 'AMZ', 'ANL', 'AOI', 'AOP', 'ARL', 'ARO',
+  'CAM', 'CAR', 'CAS', 'CGI', 'CNA', 'EAF', 'EAS', 'EAT', 'ENA', 'EPE',
+  'EPW', 'HBO', 'IND', 'MED', 'NAS', 'NAT', 'NAU', 'NEU', 'NNA', 'NNE',
+  'NNW', 'NPE', 'NPW', 'SAF', 'SAH', 'SAS', 'SAT', 'SAU', 'SEA', 'SEU',
+  'SIO', 'SPE', 'SPW', 'SSA', 'TIB', 'WAF', 'WNA', 'WPE', 'WPN', 'WPS'
+];
+
+var model_codes = [
+  'CCSM4', 'IPSL_CM5A_LR', 'MRI_CGCM3' ,'NorESM', 'BCC_CSM',
+  'IPSL_CM5A_MR', 'GISS_E2_H', 'CanESM', 'MIROC5', 'MIROC_ESM'
+];
