2d3781bd16a1cd6c8695fc4c994ac9cee3e63025
Nathan Matteson <matteson@obstructures.org>
2014-07-22 08:24:18 -0500

    Better CSV uploads (catch MS Excel bullshit)

--- emulator/views.py
+++ emulator/views.py
@@ -1,4 +1,7 @@
+import csv
+import itertools
 from datetime import datetime
+import numpy as np
 from flask import render_template, request, Blueprint, jsonify
 from emulator import Emulator
 
@@ -92,11 +95,37 @@ def global_mean(rcp, temp):
 
 @mod.route('/csv_upload', methods=['POST', 'GET', ])
 def csv_upload():
+    import StringIO
+    def _normalize_newlines(string):
+        import re
+        return re.sub(r'(\r\n|\r|\n)', '\n', string)
+
+    def split(txt, seps):
+        default_sep = seps[0]
+        for sep in seps[1:]:
+            txt = txt.replace(sep, default_sep)
+        return [i.strip() for i in txt.split(default_sep)]
+
     if request.method == 'POST':
-        csv = request.files['csv']
-        new_rcp = [float(n) for n in csv.read().split(',')]
-        print(new_rcp, len(new_rcp))
-        if len(new_rcp) != 96:
-            return 'Your CSV file needs to have 1 row of 96 values.', 501
+        l = request.files['csv'].stream.readlines()
+        l = '\n'.join(l)
+        l = _normalize_newlines(l)
+        csv_ = csv.reader(StringIO.StringIO(l))
+        new_rcp = []
+        for c in csv_:
+            new_rcp.append(c)
+        new_rcp = list(itertools.chain(*new_rcp))
+        new_rcp = [float(x) for x in new_rcp]
+        n = len(new_rcp)
+        if n > 96:
+            new_rcp = new_rcp[:96]
+        elif n < 96:
+            new_rcp = np.concatenate(
+                (
+                    np.array(new_rcp),
+                    (np.arange(1, 96 - n + 1) * (new_rcp[-1] - new_rcp[-2])) +
+                    new_rcp[-1]
+                )
+            ).tolist()
         e = do_session()['emulator']
         return jsonify(e.get_model_rcp_output(co2=new_rcp))
