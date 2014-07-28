81d9194c9f7a04accac13a625665e06e566df936
Nathan Matteson <matteson@obstructures.org>
2014-07-28 11:17:23 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = True
+ASSETS_DEBUG = False
+
+CACHE_TYPE = 'memcached'
+CACHE_KEY_PREFIX = 'emulator_dev'
+
+ADMINS = frozenset(['matteson@obstructures.org'])
+SECRET_KEY = 'REPLACEME'
---
+++ run.py
@@ -0,0 +1,5 @@
+from emulator import app
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
