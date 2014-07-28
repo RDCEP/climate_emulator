c9ac3d64238deddf08971858d4dc37242ea6d26a
Nathan Matteson <matteson@obstructures.org>
2014-07-28 10:39:38 -0500

    Add ordered list of regions

---
+++ emulator/data/mapping.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+REGION_ORDER = [
+    'GMT', 'GLL', 'GLO', 'MDE', 'ANT', 'BRA', 'CAE', 'CNA', 'SSA', 'WSA',
+    'NSA', 'CEA', 'MEX', 'EUW', 'MOG', 'NOR', 'AUS', 'KAZ', 'JAP', 'RSE',
+    'AIS', 'KOP', 'USE', 'SEA', 'EUE', 'SAL', 'SAF', 'MAD', 'IND', 'GRE',
+    'NAF', 'NWZ', 'WAF', 'CAF', 'ENE', 'USW', 'CAW', 'ALA', 'RSW', 'ENG',
+    'MED', 'CAR', 'INO', 'SPW', 'SPE', 'EPW', 'EPE', 'NPW', 'NPE', 'NAT',
+    'SAT', 'EAT', 'SIO', 'NNE', 'NNW', 'WPE', 'WPS', 'HBO', 'WPN', 'NNA',
+    'ARO', 'AOP', 'AOI',
+]
