d93f00cc881ddb80d8400cdf16ed31796dde6b31
Nathan Matteson <matteson@obstructures.org>
2014-07-28 08:56:04 -0500

    Remove FGOALS model

--- emulator/static/js/app/geopolitical/models.js
+++ emulator/static/js/app/geopolitical/models.js
@@ -1,6 +1,6 @@
 var model_codes = [
   'CCSM4', 'HadGEM2', 'NorESM', 'CanESM2', 'BCC_CSM_11', 'IPSL-CM5A-MR',
-  'CNRM-CM5', 'MIROC5', 'FGOALS', 'MIROC-ESM-CHEM', 'GISS-E2-H', 'GISS-E2-R',
+  'CNRM-CM5', 'MIROC5', 'MIROC-ESM-CHEM', 'GISS-E2-H', 'GISS-E2-R',
   'MPI-ESM-MR', 'MRI-CGCM3', 'MPI-ESM-LR'
 ];
 
