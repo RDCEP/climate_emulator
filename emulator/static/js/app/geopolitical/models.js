1353c1d0219f33715a8102f2979469839bdb8d49
Nathan Matteson <matteson@obstructures.org>
2014-07-28 19:29:09 -0500

    Parse from CSV

--- emulator/static/js/app/geopolitical/models.js
+++ emulator/static/js/app/geopolitical/models.js
@@ -1,7 +1,3 @@
 var model_codes = [
-  'CCSM4', 'HadGEM2', 'NorESM', 'CanESM2', 'BCC_CSM_11', 'IPSL-CM5A-MR',
-  'CNRM-CM5', 'MIROC5', 'MIROC-ESM-CHEM', 'GISS-E2-H', 'GISS-E2-R',
-  'MPI-ESM-MR', 'MRI-CGCM3', 'MPI-ESM-LR'
+    'bcc-csm-1.1', 'canESM2', 'CCSM4', 'CNRM-CM5', 'GISS-E2-H', 'GISS-E2-R', 'HadGEM2', 'IPSL-CM5A-MR', 'MIROC5', 'MIROC-ESM-CHEM', 'MPI-ESM-LR', 'MPI-ESM-MR', 'mri-cgcm3', 'NorESM'
 ];
-
-
