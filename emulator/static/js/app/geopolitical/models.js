41b52bc34f1bce42b848d6abe7a588a0677a3068
Nathan Matteson <matteson@obstructures.org>
2014-07-29 07:51:12 -0500

    Update model names

--- emulator/static/js/app/geopolitical/models.js
+++ emulator/static/js/app/geopolitical/models.js
@@ -1,3 +1,3 @@
 var model_codes = [
-    'bcc-csm-1.1', 'canESM2', 'CCSM4', 'CNRM-CM5', 'GISS-E2-H', 'GISS-E2-R', 'HadGEM2', 'IPSL-CM5A-MR', 'MIROC5', 'MIROC-ESM-CHEM', 'MPI-ESM-LR', 'MPI-ESM-MR', 'mri-cgcm3', 'NorESM'
+    'BCC-CSM-1.1', 'CanESM2', 'CCSM4', 'CNRM-CM5', 'GISS-E2-H', 'GISS-E2-R', 'HadGEM2', 'IPSL-CM5A-MR', 'MIROC5', 'MIROC-ESM-CHEM', 'MPI-ESM-LR', 'MPI-ESM-MR', 'MRI-CGCM3', 'NorESM'
 ];
